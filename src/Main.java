6a2b416459e013f5ef67318da80b93b62c9452ee
Matheus Rezende <matheus.rezende@unimedanapolis.com.br>
2025-08-21 14:59:51 -0300

    Commit segundo

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Filho filho = new Filho("Lucas", 155);
         Pai pai = new Pai("Marcos", "Engenheiro");
         Avo avo = new Avo("Antônio", 80);
 
-        // Testando a reflexão em diferentes objetos
         Reflexao.inspecionarClasse(filho);
-        System.out.println("\n===========================\n");
         Reflexao.inspecionarClasse(pai);
-        System.out.println("\n===========================\n");
         Reflexao.inspecionarClasse(avo);
     }
 }
--- src/Reflexao.java
+++ src/Reflexao.java
@@ -6,33 +6,36 @@ public class Reflexao {
     public static void inspecionarClasse(Object obj) {
         Class<?> clazz = obj.getClass();
 
-        System.out.println("===== INSPEÇÃO DA CLASSE: " + clazz.getSimpleName() + " =====");
+        System.out.println("\n===== INSPEÇÃO DA CLASSE: " + clazz.getSimpleName() + " =====");
 
-        // Listar atributos e métodos, subindo na hierarquia de classes
-        while (clazz != null) {
+
+        while (clazz != null && clazz != Object.class) {
             listarAtributos(clazz);
             listarMetodos(clazz);
+
+            if (clazz == Pessoa.class) {
+                break;
+            }
+
             clazz = clazz.getSuperclass();
         }
     }
 
     private static void listarAtributos(Class<?> clazz) {
         Field[] campos = clazz.getDeclaredFields();
-        if (campos.length > 0) {
             System.out.println("\nAtributos da classe " + clazz.getSimpleName() + ":");
             for (Field campo : campos) {
                 System.out.println(" - " + campo.getName() + " (" + campo.getType().getSimpleName() + ")");
             }
-        }
+
     }
 
     private static void listarMetodos(Class<?> clazz) {
         Method[] metodos = clazz.getDeclaredMethods();
-        if (metodos.length > 0) {
             System.out.println("\nMétodos da classe " + clazz.getSimpleName() + ":");
             for (Method metodo : metodos) {
                 System.out.println(" - " + metodo.getName());
             }
-        }
+
     }
 }
